b88d9bff9bc95d9b9fa45690142fda520e29ff43
Dariusz Nowak <darek.a.nowak@gmail.com>
2022-05-30 20:56:21 +0200

    Implement schema validation

--- src/main/kotlin/io/github/dnowak/jsonSchemaValidation/usecase/validateSchema.kt
+++ src/main/kotlin/io/github/dnowak/jsonSchemaValidation/usecase/validateSchema.kt
@@ -2,24 +2,28 @@ package io.github.dnowak.jsonSchemaValidation.usecase
 
 import arrow.core.Either
 import arrow.core.continuations.either
-import io.github.dnowak.jsonSchemaValidation.domain.Document
-import io.github.dnowak.jsonSchemaValidation.domain.SchemaId
-import io.github.dnowak.jsonSchemaValidation.domain.ValidateJson
+import io.github.dnowak.jsonSchemaValidation.domain.*
 import io.github.dnowak.jsonSchemaValidation.port.repository.GetSchema
-import io.github.dnowak.jsonSchemaValidation.domain.ValidateSchema as ValidateDocument
+import io.github.dnowak.jsonSchemaValidation.port.repository.GetSchemaError
+import io.github.dnowak.jsonSchemaValidation.domain.ValidateSchema as CheckSchema
 
 sealed interface ValidateSchemaError {
-
+    data class Unknown(val message: String) : ValidateSchemaError
 }
 
 typealias ValidateSchema = (SchemaId, Document) -> Either<ValidateSchemaError, Unit>
 
 suspend fun validateSchema(
     validateJson: ValidateJson,
-    validateDocument: ValidateDocument,
+    checkSchema: CheckSchema,
     getSchema: GetSchema,
     schemaId: SchemaId,
     document: Document,
 ): Either<ValidateSchemaError, Unit> = either {
-   TODO("implement")
+    val schema = getSchema(schemaId).toEither().mapLeft(::mapError).bind()
+    checkSchema(schema, document).mapLeft(::mapError).bind()
 }
+
+private fun mapError(error: GetSchemaError): ValidateSchemaError = ValidateSchemaError.Unknown(error.toString())
+
+private fun mapError(error: SchemaValidationError): ValidateSchemaError  = ValidateSchemaError.Unknown(error.toString())
